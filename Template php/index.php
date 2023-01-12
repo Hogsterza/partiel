28e927da3eded656662d99ecaf2a413167d1b9c0
Hogsterza <116167516+Hogsterza@users.noreply.github.com>
2023-01-12 16:55:08 +0100

    Add files via upload

---
+++ Template php/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gestion Utilisateurs</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+<div class="container">
+        <a href="ajouter.php" class="Btn_add"> <img src="images/plus.png"> Ajouter</a>
+        
+        <?php
+        include 'connexion.php';
+
+        $sql = "SELECT * FROM tabledatabase1";
+        $result = mysqli_query($connection, $sql);
+        ?>
+
+        <table>
+            <tr id="items">
+                <th>Nom</th>
+                <th>Prénom</th>
+                <th>Age</th>
+                <th>Modifier</th>
+                <th>Supprimer</th>
+            </tr>
+            <?php while ($row = mysqli_fetch_array($result)) {
+                echo "<tr>";
+                echo "<td>" . $row['nom'] . "</td>";
+                echo "<td>" . $row['prenom'] . "</td>";
+                echo "<td>" . $row['age'] . "</td>";
+            }
+
+
